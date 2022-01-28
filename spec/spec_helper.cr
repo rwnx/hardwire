0c03e58899514b9f508a0afbd8e5d30680ca659d
Rowan <rowan@weaver.build>
2022-01-28 15:34:38 +0000

    Add Scopes support
    * add debug logs, make tags runtime
    * add scaffold for runtime scopes
    * add compile errors for scope
    * add scope destruction, docs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -7,6 +7,6 @@ def assert_compile_error(path : String, message : String) : Nil
   buffer = IO::Memory.new
   result = Process.run("crystal", ["run", "--no-color", "--no-codegen", "spec/" + path], error: buffer)
   result.success?.should be_false
-  buffer.to_s.should contain message
+  buffer.to_s.downcase.should contain message.downcase
   buffer.close
 end
